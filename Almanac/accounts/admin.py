1e55589cd03768fe46636bc8e4314590b242edbf
miraj-code <getanehleul@gmail.com>
2022-04-28 12:28:43 +0300

    Update admin.py

--- Almanac/accounts/admin.py
+++ Almanac/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from accounts.models import CustomUser
 
-# Register your models here.
+class CustomUserAdminManager(admin.ModelAdmin):
+	readonly_fields = ['password']
+	list_display = ('username', 'first_name', 'last_name', 'is_superuser')
+	fieldsets = (
+		('Personal Informations', {
+			'fields': ('username', 
+					'first_name', 
+					'last_name', 
+					'email', 
+					'sex', 
+					'bio', 
+					'pp', 
+					'skills', 
+					'password', 
+					'date_joined')
+			}),
+		('Advanced Options', {
+			'fields': ('user_permissions', 'groups', 'is_staff', 'is_active', 'is_superuser')
+			}),
+		)
+
+admin.site.register(CustomUser, CustomUserAdminManager)
